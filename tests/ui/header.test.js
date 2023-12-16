d6105a9849175b737011743554025f457e22b1f0
100QN <83006473+100QNKolev@users.noreply.github.com>
2023-12-16 14:45:32 +0200

    Fixed Urls In Tests

--- tests/ui/header.test.js
+++ tests/ui/header.test.js
@@ -1,8 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check header', async ({ page }) => {
-    await page.goto('http://localhost:8080'); 
-    const homeLink = await page.$('p[href="/"]');
+    await page.goto('https://shadoowkrit-exam.onrender.com'); 
+    const homeLink = await page.$('a[href="/"]');
     const text = await homeLink.textContent();
     expect(text).toBe('My Shopping List for Tomorrow');
   });
